71ae458df0895b71c5797b3604deb9c3579d9c99
Mohd Daud Muhamad Syahrul Fahmie (IFMY CSC FI ADI DES) <MuhamadSyahrulFahmie.MohdDaud@infineon.com>
2023-08-09 12:50:09 +0800

    change to true

--- CordovaSecureStorageApp/www/js/index.js
+++ CordovaSecureStorageApp/www/js/index.js
@@ -19,7 +19,7 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener('deviceready', onDeviceReady, true);
 
 // function onDeviceReady() {
 //     // Cordova is now initialized. Have fun!
